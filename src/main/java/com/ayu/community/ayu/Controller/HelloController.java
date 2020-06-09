636dd264a8f333324e7d53843a137905e3aeee9f
Ayu000 <370717914@qq.com>
2020-06-09 18:28:45 +0800

    init repo

---
+++ src/main/java/com/ayu/community/ayu/Controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.ayu.community.ayu.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+
+        return "hello";
+
+    }
+}
